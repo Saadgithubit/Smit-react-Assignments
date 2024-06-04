c305032adaa1889f62213e7f46cab074c56c2fd5
Saadgithubit <saad45257@gmail.com>
2024-06-04 23:01:56 +0500

    add switch button

--- Mernstack-Project/src/Components/Navbar/navbar.js
+++ Mernstack-Project/src/Components/Navbar/navbar.js
@@ -2,6 +2,7 @@ import './navbar.css'
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2'
+import Switch from '@mui/joy/Switch';
 
 import { removetoken } from "../../Store/userToken";
 import { removeUser } from "../../Store/userSlice";
@@ -15,8 +16,10 @@ function Navbar() {
     const userToken = useSelector(state => state.userTokenReducer.tokens)
     const [isoptionclicked, setisoptionclicked] = useState(false)
     const [isButtonclicked, setisButtonclicked] = useState(false)
+    const [checked , setchecked] = useState()
     // console.log('token', userToken);
     // console.log('user', user);
+    console.log(checked);
 
     const signOut = () => {
         fetch('https://node-js-production-7fec.up.railway.app/users/logout', {
@@ -44,78 +47,84 @@ function Navbar() {
 
     return (
         <div>
-        <nav className="bg-white border-2 py-6 flex justify-around">
-            <div className="flex space-x-2 text-3xl py-2">
-                <i onClick={() => navigate('/')} class="cursor-pointer fa-regular fa-chart-bar m-1 text-blue-500"></i>
-                <h4 onClick={() => navigate('/')} className="font-bold cursor-pointer">Sapphire</h4>
-            </div>
-            <div className='flex'>
-                <ul className="flex py-3">
-                    <li onClick={() => navigate('/')} className="mx-6 hover:text-blue-600 hover:cursor-pointer">Home</li>
-                    <li className="mx-6 hover:text-blue-600 hover:cursor-pointer">Shop</li>
-                    <li className="mx-6 hover:text-blue-600 hover:cursor-pointer">Blogs</li>
-                    <li className="mx-6 hover:text-blue-600 hover:cursor-pointer">Pages</li>
-                    <li className="mx-6 hover:text-blue-600 hover:cursor-pointer">Contact</li>
-                    <li className='flex space-x-7 mx-20'>
-                        <i class="fa-solid fa-magnifying-glass hover:text-blue-600 hover:cursor-pointer"></i>
-                        <span className="flex space-x-2">
-                            <i onClick={() => {user? navigate('/cart'): signInFirst()}} class="fa-solid fa-cart-shopping hover:text-blue-600 hover:cursor-pointer"></i>
-                            <p>{totalCartProducts}</p>
-                        </span>
-                    </li>
-                </ul>
-                {!user ? <button onClick={() => navigate('/signin')} className='border-2 px-3 py-3 rounded-3xl font-bold w-40 hover:bg-blue-400 hover:text-white'>Log In</button>
-                    : <div>
-                        <button onClick={() => setisoptionclicked(!isoptionclicked)} className="border-2 bg-blue-500 text-white w-36 p-2 rounded-lg">{user.fullname} <i class="mx-1 fa-solid fa-caret-down"></i></button>
-                        {isoptionclicked && <div className="flex flex-col border-2 rounded-md w-36 absolute top-23 bg-gray-300">
-                            <button onClick={signOut} className='px-3 py-3 font-bold hover:bg-blue-400 hover:text-white'>Log Out</button>
-                            {user.email === 'saad@gmail.com' &&<div className='w-full'>
-                                <button onClick={() => navigate('/postadd')} className='w-full px-3 py-3 font-bold hover:bg-blue-400 hover:text-white'>Add Post</button>
-                                <button onClick={() => navigate('/myadds')} className='w-full px-3 py-3 font-bold hover:bg-blue-400 hover:text-white'>My Adds</button>
-                                </div>}
-                        </div>}
-
-
-                    </div>
-                }
-            </div>
-            <div>
-
-            </div>
-        </nav>
-        <nav className="hamb flex justify-between mr-3 my-2 border-t-2 border-b-2 w-full p-2">
-            <div className='flex justify-center items-center mx-4 text-xl font-bold text-gray-600'>
+            <div className='flex justify-center items-center space-x-3 font-semibold absolute top-32 right-5'>
+                {checked ? <p>Enable Light Mode</p>: <p>Enable Dark Mode</p>}
+                <Switch 
+                  onChange={(e) => setchecked(e.target.checked)}                
+                />
+                </div>
+            <nav className="bg-white border-2 py-6 flex justify-around navbar">
                 <div className="flex space-x-2 text-3xl py-2">
-                <i onClick={() => navigate('/')} class="fa-regular fa-chart-bar m-1 text-blue-500 cursor-pointer"></i>
-                <h4 onClick={() => navigate('/')} className="font-bold cursor-pointer">Sapphire</h4>
-            </div>
-            </div>
-            {user && <span className="flex items-center space-x-2">
-                            <i onClick={() => navigate('/cart')} class="fa-solid fa-cart-shopping hover:text-blue-600 hover:cursor-pointer"></i>
-                            <p>{totalCartProducts}</p>
-                        </span>}
+                    <i onClick={() => navigate('/')} class="cursor-pointer fa-regular fa-chart-bar m-1 text-blue-500"></i>
+                    <h4 onClick={() => navigate('/')} className="font-bold cursor-pointer">Sapphire</h4>
+                </div>
+                <div className='flex'>
+                    <ul className="flex py-3">
+                        <li onClick={() => navigate('/')} className="mx-6 hover:text-blue-600 hover:cursor-pointer">Home</li>
+                        <li className="mx-6 hover:text-blue-600 hover:cursor-pointer">Shop</li>
+                        <li className="mx-6 hover:text-blue-600 hover:cursor-pointer">Blogs</li>
+                        <li className="mx-6 hover:text-blue-600 hover:cursor-pointer">Pages</li>
+                        <li className="mx-6 hover:text-blue-600 hover:cursor-pointer">Contact</li>
+                        <li className='flex space-x-7 mx-20'>
+                            <i class="fa-solid fa-magnifying-glass hover:text-blue-600 hover:cursor-pointer"></i>
+                            <span className="flex space-x-2">
+                                <i onClick={() => { user ? navigate('/cart') : signInFirst() }} class="fa-solid fa-cart-shopping hover:text-blue-600 hover:cursor-pointer"></i>
+                                <p>{totalCartProducts}</p>
+                            </span>
+                        </li>
+                    </ul>
+                    {!user ? <button onClick={() => navigate('/signin')} className='border-2 px-3 py-3 rounded-3xl font-bold w-40 hover:bg-blue-400 hover:text-white'>Log In</button>
+                        : <div>
+                            <button onClick={() => setisoptionclicked(!isoptionclicked)} className="border-2 bg-blue-500 text-white w-36 p-2 rounded-lg">{user.fullname} <i class="mx-1 fa-solid fa-caret-down"></i></button>
+                            {isoptionclicked && <div className="flex flex-col border-2 rounded-md w-36 absolute top-23 bg-gray-300">
+                                <button onClick={signOut} className='px-3 py-3 font-bold hover:bg-blue-400 hover:text-white'>Log Out</button>
+                                {user.email === 'saad@gmail.com' && <div className='w-full'>
+                                    <button onClick={() => navigate('/postadd')} className='w-full px-3 py-3 font-bold hover:bg-blue-400 hover:text-white'>Add Post</button>
+                                    <button onClick={() => navigate('/myadds')} className='w-full px-3 py-3 font-bold hover:bg-blue-400 hover:text-white'>My Adds</button>
+                                </div>}
+                            </div>}
+
+
+                        </div>
+                    }
+                </div>
                 <div>
-                {!isButtonclicked?<i onClick={() => setisButtonclicked(!isButtonclicked)} class="fa-solid fa-bars text-2xl border-2 w-16 p-2 rounded-lg"></i>
-                :<i onClick={() => setisButtonclicked(!isButtonclicked)} class="fa-solid fa-xmark text-3xl border-2 w-16 p-2 rounded-lg"></i>}
+
+                </div>
+            </nav>
+            <nav className="hamb flex justify-between mr-3 my-2 border-t-2 border-b-2 w-full p-2">
+                <div className='flex justify-center items-center mx-4 text-xl font-bold text-gray-600'>
+                    <div className="flex space-x-2 text-3xl py-2">
+                        <i onClick={() => navigate('/')} class="fa-regular fa-chart-bar m-1 text-blue-500 cursor-pointer"></i>
+                        <h4 onClick={() => navigate('/')} className="font-bold cursor-pointer">Sapphire</h4>
+                    </div>
+                </div>
+                {user && <span className="flex items-center space-x-2">
+                    <i onClick={() => navigate('/cart')} class="fa-solid fa-cart-shopping hover:text-blue-600 hover:cursor-pointer"></i>
+                    <p>{totalCartProducts}</p>
+                </span>}
+                <div>
+                    {!isButtonclicked ? <i onClick={() => setisButtonclicked(!isButtonclicked)} class="fa-solid fa-bars text-2xl border-2 w-16 p-2 rounded-lg"></i>
+                        : <i onClick={() => setisButtonclicked(!isButtonclicked)} class="fa-solid fa-xmark text-3xl border-2 w-16 p-2 rounded-lg"></i>}
                 </div>
                 {isButtonclicked && <div className='absolute top-20 right-3 bg-white'>
-                <ul className="flex flex-col py-3 border-2 w-72">
-                    <li className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">
-                {user? <p className='border-b-2 p-2 font-bold text-xl w-full'>{user.fullname}</p>: <p> No User</p>}
+                    <ul className="flex flex-col py-3 border-2 w-72">
+                        <li className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">
+                            {user ? <p className='border-b-2 p-2 font-bold text-xl w-full'>{user.fullname}</p> : <p> No User</p>}
                         </li>
-                    <li onClick={() => navigate('/')} className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Home</li>
-                    <li className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Shop</li>
-                    <li className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Blogs</li>
-                    <li className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Pages</li>
-                    <li className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Contact</li>
-                    {!user?<li onClick={() => navigate('/signin')} className="h-12 font-bold flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Log In</li>
-                    :<div>
-                    <li onClick={signOut} className="h-12 font-bold flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Log Out</li>
-                    {user.email === 'saad@gmail.com' &&<div>
-                    <li onClick={() => navigate('/postadd')} className="h-12 font-bold flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Add Post</li>
-                    <li onClick={() => navigate('/myadds')} className="h-12 font-bold flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">My Adds</li>
-                    </div>}
-                    </div>}
+                        <li onClick={() => navigate('/')} className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Home</li>
+                        <li className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Shop</li>
+                        <li className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Blogs</li>
+                        <li className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Pages</li>
+                        <li className="h-12 flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Contact</li>
+                        {!user ? <li onClick={() => navigate('/signin')} className="h-12 font-bold flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Log In</li>
+                            : <div>
+                                <li onClick={signOut} className="h-12 font-bold flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Log Out</li>
+                                {user.email === 'saad@gmail.com' && <div>
+                                    <li onClick={() => navigate('/postadd')} className="h-12 font-bold flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">Add Post</li>
+                                    <li onClick={() => navigate('/myadds')} className="h-12 font-bold flex justify-center items-center hover:text-blue-600 hover:bg-gray-400 hover:cursor-pointer">My Adds</li>
+                                </div>}
+                            </div>}
                     </ul>
                 </div>}
             </nav>
